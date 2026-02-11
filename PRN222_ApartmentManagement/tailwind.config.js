f63e1c037071f68c416ebf05c4667cb8ceb88d3e
minhgl <45535771+rinkito5@users.noreply.github.com>
2026-02-11 10:59:47 +0700

    create components

---
+++ PRN222_ApartmentManagement/tailwind.config.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+  content: [
+    "./Pages/**/*.{cshtml,razor,js}",
+    "./Pages/Shared/**/*.{cshtml,razor,js}",
+    "./wwwroot/**/*.{html,js}",
+    "./Views/**/*.{cshtml,razor}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
