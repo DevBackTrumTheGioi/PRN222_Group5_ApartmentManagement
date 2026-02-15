e4bee789ba61591c6b1e9883d0f7c10b7a05ce5b
minhgl <45535771+rinkito5@users.noreply.github.com>
2026-02-15 15:31:06 +0700

    index page

---
+++ PRN222_ApartmentManagement/Pages/BQL_Manager/Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PRN222_ApartmentManagement.Pages.BQL_Manager;
+
+[Authorize(Roles = "BQL_Manager")]
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
---
+++ PRN222_ApartmentManagement/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+using PRN222_ApartmentManagement.Models;
+
+namespace PRN222_ApartmentManagement.Pages;
+
+public class IndexModel : PageModel
+{
+    public IActionResult OnGet()
+    {
+        if (!User.Identity?.IsAuthenticated ?? true)
+        {
+            return RedirectToPage("/Account/Login");
+        }
+
+        var role = User.FindFirstValue(ClaimTypes.Role);
+
+        return role switch
+        {
+            nameof(UserRole.Admin) => RedirectToPage("/Admin/Index"),
+            nameof(UserRole.BQL_Manager) => RedirectToPage("/BQL_Manager/Index"),
+            nameof(UserRole.BQL_Staff) => RedirectToPage("/BQL_Staff/Index"),
+            nameof(UserRole.Resident) => RedirectToPage("/Resident/Index"),
+            nameof(UserRole.BQT_Head) => RedirectToPage("/BQT_Head/Index"),
+            nameof(UserRole.BQT_Member) => RedirectToPage("/BQT_Member/Index"),
+            _ => RedirectToPage("/Account/Login")
+        };
+    }
+}
+
